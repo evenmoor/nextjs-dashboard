1668d70a599bd4bde6232543b4a450455a560435
evenmoor <evenmoor4free@gmail.com>
2024-02-21 23:45:24 -0700

    chapter 11
    Finished up to chapter 11

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -39,6 +39,13 @@ export type LatestInvoice = {
   amount: string;
 };
 
+export type cardData = {
+  totalPaidInvoices: string;
+  totalPendingInvoices: string;
+  numberOfInvoices: number;
+  numberOfCustomers: number;
+}
+
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
